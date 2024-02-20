eeeb0fcf2295443426323acfb08e39ebba641867
liyuke12580 <liyuke65535@gmail.com>
2024-02-20 13:29:06 +0000

    env

--- enviroments.sh
+++ enviroments.sh
@@ -1,4 +1,4 @@
-pip3 install torch torchvision torchaudio
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
 pip install einops
 pip install timm
 pip install scikit-image
