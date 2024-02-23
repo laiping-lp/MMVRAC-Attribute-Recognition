0f789d38dc0839da1862dedc3704b17fbc2c3b91
liyuke12580 <liyuke65535@gmail.com>
2024-02-23 02:34:58 +0000

    update torch version

--- enviroments.sh
+++ enviroments.sh
@@ -1,4 +1,4 @@
-pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install torch==1.13.0+cu116 torchvision==0.14.0+cu116 torchaudio==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu116
 pip install einops
 pip install timm
 pip install scikit-image
